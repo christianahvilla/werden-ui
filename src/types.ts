d952419c1455f1212763820e11bcfae6e23e3d99
Christian Alejandro Herrejon Villa <christianahvilla@gmail.com>
2022-10-13 18:31:52 -0500

    [FEAT]: Add sorting for props/keys (#2)

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export type KIND_TYPE = 'info' | 'success' | 'warning' | 'error'
+
+export type PLACEMENT_TYPE =
+  | 'topCenter'
+  | 'bottomCenter'
+  | 'topStart'
+  | 'topEnd'
+  | 'bottomStart'
+  | 'bottomEnd'
+
+export type APPEARENCE_TYPE =
+  | 'default'
+  | 'primary'
+  | 'link'
+  | 'subtle'
+  | 'ghost'
